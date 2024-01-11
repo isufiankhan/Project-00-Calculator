98bde724fc9bb49dcfd4ad3d2181ae0a2c8ec61c
M Sufian Khan <155994608+isufiankhan@users.noreply.github.com>
2024-01-11 14:57:54 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,44 @@
+import inquirer from "inquirer";
+
+const answers:{
+    NumberOne:number,
+    NumberTwo:number,
+    operator:string
+} = await inquirer.prompt([
+    {
+        name: "NumberOne",
+        type: "number",
+        message: "Enter your First Number:",
+    },
+    {
+        name: "NumberTwo",
+        type: "number",
+        message: "Enter your Second Number:",
+    },
+    {
+        name: "operator",
+        type: "list",
+        choices: ["Addition(+)","Subtraction(-)","Multiplication(*)","Division(/)"],
+        message: "Select your Operator:"
+    }
+])
+
+const{NumberOne,NumberTwo,operator} = answers
+if(NumberOne && NumberTwo && operator){
+    let result:number=0;
+    if(operator == "Addition(+)"){
+        result = NumberOne + NumberTwo
+    }
+    else if(operator == "Subtraction(-)"){
+        result = NumberOne - NumberTwo
+    }
+    else if(operator == "Multiplication(*)"){
+        result = NumberOne * NumberTwo
+    }
+    else if(operator == "Division(/)"){
+        result = NumberOne / NumberTwo
+    }
+
+    console.log("Your Result is :",result);
+}
+else{console.log("Kindly Enter the valid Input!")}
